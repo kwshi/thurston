ebb3b0e424cff1323972d82e0e6d3dda8beba734
Kye Shi <shi.kye@gmail.com>
2023-06-09 00:30:34 -0700

    implement polygon drawing tool

--- src/component/Thurston.svelte
+++ src/component/Thurston.svelte
@@ -15,6 +15,9 @@
     DrawCurve,
   }
 
+  let toolMode = ToolMode.Polygon;
+  let toolDraw: { polygon: Complex.Complex[]; done: boolean } | null = null;
+
   let stuff = Graph.Example.rectangle(10, 30);
   let graph = stuff.graph;
   let origin = Graph.interior(graph).next().value!;
@@ -52,12 +55,75 @@
         (node) => Complex.abs(node.label.position) + node.label.radius
       )
     );
+
+  const getPosition = (ev: MouseEvent, unit: number) =>
+    Complex.scaleInv(
+      Complex.complex(
+        ev.offsetX - viewportWidth / 2,
+        -(ev.offsetY - viewportHeight / 2)
+      ),
+      unit
+    );
+
+  const mouseDown = (ev: MouseEvent & { currentTarget: SVGElement }) => {
+    const pos = getPosition(ev, unitSizeE);
+    switch (toolMode) {
+      case ToolMode.Polygon:
+        toolDraw = { polygon: [pos], done: false };
+        break;
+    }
+  };
+
+  const mouseMove = (ev: MouseEvent) => {
+    const pos = getPosition(ev, unitSizeE);
+    switch (toolMode) {
+      case ToolMode.Polygon:
+        if (!toolDraw || toolDraw.done) return;
+        toolDraw = { ...toolDraw, polygon: [...toolDraw.polygon, pos] };
+    }
+  };
+
+  const mouseUp = () => {
+    switch (toolMode) {
+      case ToolMode.Polygon:
+        if (toolDraw) toolDraw = { ...toolDraw, done: true };
+    }
+  };
+
+  const mouseLeave = () => {
+    if (toolDraw && !toolDraw.done) toolDraw = null;
+  };
+
+  const toCanvas = (z: Complex.Complex) =>
+    Complex.add(
+      Complex.scale(Complex.conj(z), unitSizeE),
+      Complex.complex(viewportWidth / 2, viewportHeight / 2)
+    );
 </script>
 
 <div class="layout">
   <div class="top" />
   <div>
-    <svg width="100%" height="100%">
+    <svg
+      width="100%"
+      height="100%"
+      on:mousedown={mouseDown}
+      on:mouseup={mouseUp}
+      on:mousemove={mouseMove}
+      on:mouseleave={mouseLeave}
+    >
+      {#if toolDraw}
+        {@const first = toCanvas(toolDraw.polygon[0] ?? Complex.zero)}
+        {@const pathD =
+          `M${first.x} ${first.y}` +
+          toolDraw.polygon
+            .slice(1)
+            .map(toCanvas)
+            .map((p) => `L${p.x} ${p.y}`)
+            .join("") +
+          (toolDraw.done ? "Z" : "")}
+        <path d={pathD} stroke="red" fill={toolDraw.done ? "silver" : "none"} />
+      {/if}
       {#each [...euclidean.replace.values()] as node}
         <circle
           on:mouseenter={() => void (hoverCircle = node.id)}
